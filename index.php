de4512096c2f5a3b67cb6329cf795bb0443e69b0
ueslei <uesleijf@gmail.com>
2019-11-20 21:29:31 -0300

    - refactory index.php to connect create/cadastrar.php and list db mysql in table

--- index.php
+++ index.php
@@ -12,11 +12,52 @@
         <div class="container">
           <div class="jumbotron">
             <div class="row">
-                <h2>Crud <span class="badge badge-secondary">v 1.0.0 UesleiJF.</span></h2>
+                <h2>CRUD em PHP <span class="badge badge-secondary">v 1.0.0 Glaucia</span></h2>
             </div>
           </div>
             </br>
             <div class="row">
+                <p>
+                    <a href="create.php" class="btn btn-success">Adicionar</a>
+                </p>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Endereço</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Ação</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        include 'banco.php';
+                        $pdo = Banco::conectar();
+                        $sql = 'SELECT * FROM pessoa ORDER BY id DESC';
+
+                        foreach($pdo->query($sql)as $row)
+                        {
+                            echo '<tr>';
+                                  echo '<th scope="row">'. $row['id'] . '</th>';
+                            echo '<td>'. $row['nome'] . '</td>';
+                            echo '<td>'. $row['endereco'] . '</td>';
+                            echo '<td>'. $row['telefone'] . '</td>';
+                            echo '<td>'. $row['email'] . '</td>';
+                            echo '<td width=250>';
+                            echo '<a class="btn btn-primary" href="read.php?id='.$row['id'].'">Info</a>';
+                            echo ' ';
+                            echo '<a class="btn btn-warning" href="update.php?id='.$row['id'].'">Atualizar</a>';
+                            echo ' ';
+                            echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Excluir</a>';
+                            echo '</td>';
+                            echo '</tr>';
+                        }
+                        Banco::desconectar();
+                        ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
