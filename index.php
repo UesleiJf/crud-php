44ffadb0584f0bdf79a9491e71a9821d925e020a
ueslei <uesleijf@gmail.com>
2019-11-20 21:19:05 -0300

    - refactory index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Página Inicial</title>
+</head>
+
+<body>
+        <div class="container">
+          <div class="jumbotron">
+            <div class="row">
+                <h2>Crud <span class="badge badge-secondary">v 1.0.0 UesleiJF.</span></h2>
+            </div>
+          </div>
+            </br>
+            <div class="row">
+            </div>
+        </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
