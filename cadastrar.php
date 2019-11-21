02308ecc5bf447a1afd6912357e57c717c6bf872
ueslei <uesleijf@gmail.com>
2019-11-20 21:24:23 -0300

    - Create page cadastrar.php to add new contact

---
+++ cadastrar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Adicionar Contato</title>
+</head>
+
+<body>
+    <form class="form-horizontal" action="cadastrar.php" method="post">
+
+    <label>Nome</label>
+    <div>
+        <input size="50" name="nome" type="text">
+    </div>
+    <button type="submit" class="btn btn-success">Adicionar</button>
+    </form>
+</body>
+
+</html>
